4b555f351568aadd9025c4cfd3ca5036b19f18e5
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-06 22:13:44 +0100

    remove redundant and non-stubbed tests from game_spec.rb

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -33,7 +33,6 @@ feature 'Playing a game' do
 
   context "when a hit occurs" do
     scenario "'HIT!' is printed to the screen" do
-      game = Game.new Player, Board
       allow_any_instance_of(Board).to receive(:receive_shot).with(:B4).and_return(:hit)
       fill_in "coordinates", with: "B4"
       click_on "FIRE!"
@@ -43,28 +42,10 @@ feature 'Playing a game' do
 
   context 'when user submits a successful coordinate in lowercase' do
     scenario "'HIT!' is printed to the screen" do
-      game = Game.new Player, Board
       allow_any_instance_of(Board).to receive(:receive_shot).with(:B4).and_return(:hit)
       fill_in "coordinates", with: "b4"
       click_on "FIRE!"
       expect(page).to have_content 'HIT!!'
-    end
-  end
-
-  context "when there is a ship on the board" do 
-    scenario "'HIT!!' is printed on the screen when it is hit" do
-      $game.player_2.place_ship(Ship.cruiser, :B4)
-      fill_in "coordinates", with: "b4"
-      click_on "FIRE!"
-      expect(page).to have_content 'HIT!!'
-    end
-
-    scenario "'MISS!!' is printed on the screen when a shot misses it" do
-      game = Game.new Player, Board
-      game.player_2.place_ship(Ship.cruiser, :B4)
-      fill_in "coordinates", with: "j4"
-      click_on "FIRE!"
-      expect(page).to have_content 'MISS!!'
     end
   end
 
